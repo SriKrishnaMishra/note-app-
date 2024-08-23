938feb4bdff2b70204fe4ef2914ef226e5e9aebe
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-08-23 10:38:58 +0000

    good project

--- script.js
+++ script.js
@@ -34,3 +34,63 @@ const contentData = data.map((item) => item.content);
 localStorage.setItem("notes", JSON.stringify(contentData));
 
 };
+
+const addNote = (text = "", title = "") => {
+
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+  div class = "icon">
+  <i class = "save fas fa-save" style = "color:yellow">
+
+  </i>
+  <i class="trash fas fa-trash" style = "color:yellow">
+
+  </i>
+  </div>
+  <div class="title-div">
+  <textarea class="title" placeholder ="write the title ..." >${title}
+  </textarea>
+  </div>
+  <textarea class="content" placeholder ="Note down your thought..."> ${text}
+   </textarea>
+  `;
+
+
+function handleTranshClick() {
+
+  note.remove();
+  saveNotes();
+}
+
+function handleSaveClick() {
+
+  saveNotes();
+}
+
+const delBtn = note.querySelector(".trash");
+const saveButton = note.querySelector(".save");
+const textarea = note.querySelectorAll("textarea");
+
+delBtn.addEventListener("click", handleTranshClick);
+saveButton.addEventListener("click", handleSaveClick);
+main.appendChild(note);
+
+saveNotes();
+
+};
+
+function landNotes() {
+
+  const titleData = JSON.parse(localStorage.getItem("titles"))  || [];
+
+  const contentData = JSON.parse(localStorage.getItem("notes")) || [];
+
+  for(let i = 0; i < Math.max(titleData.length, contentData.length); i++) {
+
+    addNote(contentData[i], titleData[i]);
+
+  }
+}
+
+loadNotes();
